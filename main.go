905b10efd7ddc0e174be27c483cd4341bfbecf21
ahmadnadeem6236 <ahmadnadeem6236@gmail.com>
2024-10-16 18:41:34 +0530

    go init

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("### GoLang TodoList App ###")
+
+	fmt.Println()
+	fmt.Println("Study")
+	fmt.Println("Watch Golang tutorial.")
+	fmt.Println("Study about microservices")
+
+	fmt.Println()
+	fmt.Println("Work")
+	fmt.Println("Create Golang Todo App")
+	fmt.Println("Create microservices app for demonstartion.")
+
+	fmt.Println()
+	fmt.Println("Fun")
+	fmt.Println("Write story about Golang Journey")
+	fmt.Println("Create fun AI app")
+
+}
