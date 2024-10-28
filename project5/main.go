17b1db320bba7adc05781dd1867d9c34755e605b
ahmadnadeem6236 <ahmadnadeem6236@gmail.com>
2024-10-28 17:26:14 +0530

    project5: completed

--- project5/main.go
+++ project5/main.go
@@ -1,33 +1,43 @@
 package main
 
 import (
-	"errors"
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
+
+	"example.com/note/note"
 )
 
 func main() {
-	getNoteData()
+	title, content := getNoteData()
+	userNote, err := note.New(title, content)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	userNote.Display()
+	userNote.Save()
 }
 
-func getNoteData() (string, string, error) {
-	tile, err := getUserInput("Note title: ")
-	if err != nil {
-		return "", "", err
-	}
-	content, err := getUserInput("Note content: ")
-	if err != nil {
-		return "", "", err
-	}
-	return tile, content, nil
+func getNoteData() (string, string) {
+	tile := getUserInput("Note title: ")
+	content := getUserInput("Note content: ")
+	return tile, content
 }
 
-func getUserInput(prompt string) (string, error) {
+func getUserInput(prompt string) string {
 	fmt.Print(prompt)
-	var value string
-	fmt.Scanln(&value)
-
-	if value == "" {
-		return "", errors.New("please enter the values")
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		return ""
 	}
-	return value, nil
+
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+
+	return text
 }
---
+++ project5/note/note.go
@@ -0,0 +1,45 @@
+package note
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+type Note struct {
+	Title     string
+	Content   string
+	CreatedAt time.Time
+}
+
+func New(title, content string) (Note, error) {
+	if title == "" || content == "" {
+		return Note{}, errors.New("invalid note")
+	}
+
+	return Note{
+		Title:     title,
+		Content:   content,
+		CreatedAt: time.Now(),
+	}, nil
+}
+
+func (note Note) Display() {
+	fmt.Printf("You note titled %v has the following content:\n%v\n", note.Title, note.Content)
+}
+
+func (note Note) Save() {
+	filename := strings.Replace(note.Title, " ", "_", -1)
+	filename = strings.ToLower(filename) + ".json"
+
+	json, err := json.Marshal(note)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	os.WriteFile(filename, json, 0644)
+}
